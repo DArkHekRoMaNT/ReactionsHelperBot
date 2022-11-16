164c72592805a272ef9eff23c4aaa03e9e85594f
DArkHekRoMaNT <darkhekromant@yandex.ru>
2022-11-16 22:36:14 +0300

    remove file logger

--- reactionshelperbot/logger.py
+++ reactionshelperbot/logger.py
@@ -4,18 +4,11 @@ import logging
 def setup_logger():
     dt_fmt = '%Y-%m-%d %H:%M:%S'
     formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-    file_handler = logging.FileHandler(
-        filename='discord.log',
-        encoding='utf-8',
-        mode='w'
-    )
-    file_handler.setFormatter(formatter)
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
 
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    logger.addHandler(file_handler)
 
     logging.getLogger('discord').setLevel(logging.DEBUG)
     logging.getLogger('reactionshelperbot').addHandler(stream_handler)
